e0fc04cf2c314c01750613cb7caa2d5e0206f5ca
Joemar <jbagalanon@gmail.com>
2020-02-20 14:51:26 +0800

    Add Index ServiceTypes

---
+++ Pages/ServiceTypes/Create.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class CreateModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Page();
+
+        }
+    }
+}
