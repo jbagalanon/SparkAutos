1fe33d54a6d23ac4dd0f0a7f7b044c0d3952e5ee
Joemar <jbagalanon@gmail.com>
2020-02-21 21:45:52 +0800

    Added Register Model

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SparkAuto.Models
+{
+
+    //application user is extended identity user is a user account build by microsoft
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+
+        //push this file to database. Go to application dbcontext
+        //add all this file in register.cshtml.cs
+
+    }
+}
