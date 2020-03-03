66e72ca221e0f4cc2efe246b7d4c845943eb53fa
Joemar <jbagalanon@gmail.com>
2020-03-04 03:42:52 +0800

    Added Car Index.. Unfinish

---
+++ Pages/Cars/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SparkAuto.Data;
+using SparkAuto.Models.ViewModel;
+
+namespace SparkAuto.Pages.Cars
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public  CarAndCustomerViewModel CarAndCustomerVM { get; set; }
+        public void OnGet()
+        {
+
+        }
+    }
+}
