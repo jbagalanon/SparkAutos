a05fd25f839945cb0701c68568748263bba44c1f
Joemar <jbagalanon@gmail.com>
2020-03-05 13:37:03 +0800

    Added Edit and Delete Model

---
+++ Pages/Cars/Delete.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.Cars
+{
+    public class DeleteModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+
+            _db = db;
+        }
+
+        [BindProperty]
+        public Car Car { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Car = await _db.Car.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (Car == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Car = await _db.Car.FindAsync(id);
+
+            if (Car != null)
+            {
+                _db.Car.Remove(Car);
+                await _db.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Cars/Details.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.Cars
+{
+    public class DetailsModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public DetailsModel(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+
+        [BindProperty]
+        public  Car Car { get; set; }
+
+        public async Task <IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Car = await _db.Car.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (Car == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+
+        }
+
+
+        /* this is a generated model of MVC
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ServiceType = await _db.ServiceType.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (ServiceType == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+        */
+    }
+}
