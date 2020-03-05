07bffe13efe000b8f12e69d2387ed82026113c99
Joemar <jbagalanon@gmail.com>
2020-03-06 05:48:06 +0800

    Added Service Details , headers, shoppingcart and  CarServiceViewModel

---
+++ Models/ServiceShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Models
+{
+    public class ServiceShoppingCart
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int ServiceTypeId { get; set; }
+
+
+        //virtual because this is foreign key
+        [ForeignKey("CarId")]
+        public virtual Car Car { get; set; }
+
+        [ForeignKey("ServiceTypeId")]
+        public virtual  ServiceType ServiceType { get; set; }
+    }
+}
