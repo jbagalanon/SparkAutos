87d671871e8b0e449f1e670206d92c5efa7f69c1
Joemar <jbagalanon@gmail.com>
2020-03-08 02:56:58 +0800

    Added Create and History  Service Model

--- Pages/Services/Create.cshtml.cs
+++ Pages/Services/Create.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -18,28 +19,30 @@ namespace SparkAuto.Pages.Services
         {
 
             _db = db;
-            
+
         }
 
         [BindProperty]
         public CarServiceViewModel CarServiceVM { get; set; }
 
-        public async  Task<IActionResult> OnGet( int carId)
+
+        //this is for the car service model
+        public async Task<IActionResult> OnGet(int carId)
         {
             CarServiceVM = new CarServiceViewModel()
             {
                 Car = await _db.Car.Include(c => c.ApplicationUser).FirstOrDefaultAsync(c => c.Id == carId),
-                ServiceHeader =  new Models.ServiceHeader()
+                ServiceHeader = new Models.ServiceHeader()
             };
             List<string> lstServiceTypeInShoppingCard = _db.ServiceShoppingCart
                 .Include(c => c.ServiceType)
                 .Where(c => c.CarId == carId)
                 .Select(s => s.ServiceType.Name)
                 .ToList();
-                
+
             IQueryable<ServiceType> lstService = from s in _db.ServiceType
-                where !(lstServiceTypeInShoppingCard.Contains(s.Name))
-                select s;
+                                                 where !(lstServiceTypeInShoppingCard.Contains(s.Name))
+                                                 select s;
 
             CarServiceVM.ServiceTypeList = lstService.ToList();
 
@@ -55,6 +58,78 @@ namespace SparkAuto.Pages.Services
             }
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                CarServiceVM.ServiceHeader.DateAdded = DateTime.Now;
+                CarServiceVM.ServiceShoppingCart = _db.ServiceShoppingCart.Include(c => c.ServiceType)
+                    .ToList();
+
+                foreach (var item in CarServiceVM.ServiceShoppingCart)
+                {
+                    CarServiceVM.ServiceHeader.TotalPrice += item.ServiceType.Price;
+                }
+
+                CarServiceVM.ServiceHeader.CarId = CarServiceVM.Car.Id;
+
+                _db.ServiceHeader.Add(CarServiceVM.ServiceHeader);
+                await _db.SaveChangesAsync();
+
+                foreach (var detail in CarServiceVM.ServiceShoppingCart)
+                {
+                    ServiceDetails serviceDetails = new ServiceDetails
+                    {
+                        ServiceHeaderId = CarServiceVM.ServiceHeader.Id,
+                        ServiceName = detail.ServiceType.Name,
+                        ServicePrice = detail.ServiceType.Price,
+                        ServiceTypeId = detail.ServiceTypeId
+                    };
+                    _db.ServiceDetails.Add(serviceDetails);
+                }
+
+                _db.ServiceShoppingCart.RemoveRange(CarServiceVM.ServiceShoppingCart);
+
+                await _db.SaveChangesAsync();
+                return RedirectToPage("../Cars/Index", new {userId = CarServiceVM.Car.UserId});
+            }
+
+            return Page();
+
+
+        }
+
+        //handler to add the cart
+        public async Task<IActionResult> OnPostAddToCart()
+        {
+            ServiceShoppingCart objServiceCart = new ServiceShoppingCart()
+            {
+                CarId = CarServiceVM.Car.Id,
+                //binding the bakcgroun asp-for=servicetypeid
+                ServiceTypeId = CarServiceVM.ServiceDetails.ServiceTypeId,
+
+            };
+            _db.ServiceShoppingCart.Add(objServiceCart);
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Create", new {carId = CarServiceVM.Car.Id});
+        }
+        //remove cart
+
+        //handler to add the cart
+        public async Task<IActionResult> OnPostRemoveFromCart(int serviceTypeId)
+        {
+            ServiceShoppingCart objServiceCart = _db.ServiceShoppingCart.FirstOrDefault
+                (c => c.CarId == CarServiceVM.Car.Id && c.ServiceTypeId == serviceTypeId);
+           
+            _db.ServiceShoppingCart.Remove(objServiceCart);
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Create", new { carId = CarServiceVM.Car.Id });
         }
     }
 }
---
+++ Pages/Services/History.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.Services
+{
+    public class HistoryModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public HistoryModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public List<ServiceHeader> ServiceHeader { get; set; }
+        
+        public async Task OnGet(int carId)
+        {
+            ServiceHeader =await  _db.ServiceHeader.Include(c => c.Car).Include(u => u.Car.ApplicationUser)
+                .Where(c => c.CarId == carId)
+                .ToListAsync();
+        }
+    }
+}
